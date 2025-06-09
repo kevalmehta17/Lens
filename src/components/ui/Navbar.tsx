bd22ded3611cdeac0e8b6cf54d0e743f9d14238d
Keval <kevalmehta885@gmail.com>
2025-06-09 15:18:44 +0530

    modified

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import DesktopNavbar from "../DesktopNavbar";
 import MobileNavbar from "../MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser();
   return (
     <nav className="sticky backdrop-blur  supports-[backdrop-filter]:bg-background/60 z-50 top-0 w-full border-b bg-background/95">
       <div className="max-w-7xl mx-auto px-4">
